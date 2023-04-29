ee03e96eeb1cbd56652e9a785813a6f94a07a659
Aaron Hong <ahong02@uw.edu>
2023-04-28 17:56:37 -0700

    worked on wordcount

--- hw3/wordcount.c
+++ hw3/wordcount.c
@@ -4,15 +4,48 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
+	char opt[];
+	int start;
 	if(argc < 2){
 		fprintf(stderr, "Usage: %s requires an input file.\n", argv[0]);
 		return 1;
 	}
-	if(strcmp(argv[1], "-l")==0){
-		lc = true;
+	if(argv[1] == "-l" || argv[1] == "-w" || argv[1] == "-c"){
+		opt = argv[1];
+		start = 2;
+	} else {
+		opt = ' ';
+		start = 1;
 	}
-	if(strcmp(argv[1], "-w")==0){
-		wc = true;
+	for(int i = start; i < argc; i++){
+		FILE* fp = fopen(argv[i], "r");
+		process_file(fp, opt);
 	}
 	return 0;
+}
+
+void process_file(FILE* fp, char opt[]){
+	int l, w, c;
+	char ch;
+	l = 0;
+	w = 0;
+	c = 0;
+	if(fp != NULL){
+		while ((ch = getc(fp)) != EOF){
+			if(ch == '\n'){
+				l++;	
+			}
+			if(ch == ' ' || ch == '\n'){
+				w++;
+			}
+			if(ch != ' ' && ch != '\n'){
+				c++;
+			}
+		}
+		switch(opt){
+			case "-l":	
+		}
+	} else {
+		printf("%s will not open. Skipping.\n", argv[i]);
+	}
 }
